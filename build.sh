81ffa348b580819a4fd24e891b205665f7f252de
Jackson Owens <jackson_owens@alumni.brown.edu>
2022-11-06 11:26:22 -0500

    build.sh: fix up for recent Go versions

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-go install github.com/jbowens/codenamesgreen/cmd/greenapid
+go install github.com/jbowens/codenamesgreen/cmd/greenapid@latest
 parcel build src/index.html
 cp -R src/images dist/
 cp src/robots.txt dist/
